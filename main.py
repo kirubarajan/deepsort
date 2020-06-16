92ff3d4986f6a34ed22be2157d9bfaa598f2ba18
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-06-16 18:00:33 -0400

    Add default tensor type

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ For running experiments.
 from experiments import evaluate, report_metrics, train
 
 if __name__ == "__main__":
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
     train.run()
